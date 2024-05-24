3d07b7c11664b8423aa5524a0086cb9e28e8b82d
florianschrottenbaum <sf3826@mci4me.at>
2024-05-24 12:18:45 +0200

    last improvements

--- 4/main.py
+++ 4/main.py
@@ -1,14 +1,13 @@
 import streamlit as st
 import read_pandas
 import my_plot
-#Streamlit Einstellungen
-st.header("Power-Curve log.")
 #Daten einlesen und weitergeben
 df = read_pandas.read_my_csv_Activity()
-#Plot erstellen
+#figures for streamlit
 fig = my_plot.PowerCurve1(df)
 fig2 = my_plot.PowerCurve2(df)
-#Plot anzeigen
+#streamlit
+st.header("Power-Curve log.")
 st.plotly_chart(fig)
-st.header("Power-Curve linear.")
+st.header("Power-Curve linear")
 st.plotly_chart(fig2)
