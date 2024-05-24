86fcfee8baab118f6303219e4796a1f0e6ed2d1b
florianschrottenbaum <sf3826@mci4me.at>
2024-05-24 12:12:43 +0200

    last fixes

--- 4/main.py
+++ 4/main.py
@@ -2,10 +2,13 @@ import streamlit as st
 import read_pandas
 import my_plot
 #Streamlit Einstellungen
-st.header("Power-Curve")
+st.header("Power-Curve log.")
 #Daten einlesen und weitergeben
 df = read_pandas.read_my_csv_Activity()
 #Plot erstellen
-fig = my_plot.PowerCurve(df)
+fig = my_plot.PowerCurve1(df)
+fig2 = my_plot.PowerCurve2(df)
 #Plot anzeigen
 st.plotly_chart(fig)
+st.header("Power-Curve linear.")
+st.plotly_chart(fig2)
--- 4/my_plot.py
+++ 4/my_plot.py
@@ -4,11 +4,22 @@ import numpy as np
 import plotly.express as px
 from read_pandas import read_my_csv_Activity, create_pow_curve
 
-def PowerCurve(datas):
+def PowerCurve1(datas):
     #Daten einlesen
     arr = np.arange(1, len(datas["PowerOriginal"]))
     #Plot erstellen
     var = create_pow_curve(datas,arr,1)
     fig = px.line(datas, x = var["Zeit in s"], y = var["Leistung in W"])
-    fig.update_layout(title='Power Curve', xaxis_title='Time [s]', yaxis_title='Power [W]')
+    fig.update_layout(xaxis_title='Time [s]', yaxis_title='Power [W]')
+    fig.update_layout(xaxis=dict(type='log', title='Time [s]'))
     return fig
+def PowerCurve2(datas):
+    #Daten einlesen
+    arr = np.arange(1, len(datas["PowerOriginal"]))
+    #Plot erstellen
+    var = create_pow_curve(datas,arr,1)
+    fig = px.line(datas, x = var["Zeit in s"], y = var["Leistung in W"])
+    fig.update_layout(xaxis_title='Time [s]', yaxis_title='Power [W]')
+    return fig
+
+
