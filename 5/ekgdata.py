cc133c78916b39ab5371f89b648759d59fe5898e
TobiW3 <wt1651@mci4me.at>
2024-05-27 12:14:46 +0200

    updated ekgdata

--- 5/ekgdata.py
+++ 5/ekgdata.py
@@ -3,15 +3,15 @@ import pandas as pd
 from scipy.signal import find_peaks
 import plotly.express as px
 import plotly.graph_objects as go
+from person import Person
+from datetime import datetime
 
 # %% Objekt-Welt
 
 # Klasse EKG-Data für Peakfinder, die uns ermöglicht peaks zu finden
 
 class EKGdata:
-
 ## Konstruktor der Klasse soll die Daten einlesen
-
     def __init__(self, ekg_dict):
         pass
         self.id = ekg_dict["id"]
@@ -23,41 +23,62 @@ class EKGdata:
         self.fig = self.plot_time_series()
 
     def find_peaks_ekg(self):
-        peaks, properties = find_peaks(self.df['EKG in mV'], height=340, distance=20)
+        peaks, properties = find_peaks(self.df['EKG in mV'], height=330, distance=200,prominence = 60 )
         return peaks,properties
     
     def estimate_hr(self):
-        estimated_hr = ((len(self.peaks)) / (len(self.df['Time in ms'])/1000)*60)
-        return estimated_hr
+        self.estimated_hr_list = []
+        peaks_times = self.df['Time in ms'][self.peaks]
+        for i in range(1, len(peaks_times)):
+            difference = (peaks_times.iloc[i] - peaks_times.iloc[i-1])/500*60
+            self.estimated_hr_list.append(difference)
+        self.estimated_hr = sum(self.estimated_hr_list)/len(self.estimated_hr_list)
+        return self.estimated_hr
     
     def plot_time_series(self):
-        fig = go.Figure()
-        fig.add_trace(go.Scatter(x=self.df['Time in ms'], y=self.df['EKG in mV'], mode='lines', name='EKG Signal'))
-        fig.add_trace(go.Scatter(x=self.df['Time in ms'][self.peaks], y=self.df['EKG in mV'][self.peaks], mode='markers', name='Peaks', marker=dict(color='red', size=10, symbol='x')))
-        fig.update_layout(title='Peaks im EKG-Signal',
+        self.fig = go.Figure()
+        # EKG-Signal auf der linken y-Achse
+        self.fig.add_trace(go.Scatter(x=self.df['Time in ms'], y=self.df['EKG in mV'], mode='lines', name='EKG Signal', yaxis='y1'))
+        # Peaks auf der linken y-Achse
+        self.fig.add_trace(go.Scatter(x=self.df['Time in ms'][self.peaks], y=self.df['EKG in mV'][self.peaks], mode='markers', name='Peaks', marker=dict(color='red', size=10, symbol='x'), yaxis='y1'))
+        # Geschätzte Herzfrequenz auf der rechten y-Achse
+        self.fig.add_trace(go.Scatter(x=self.df['Time in ms'][self.peaks], y=self.estimated_hr_list, mode='lines', name='Estimated Heartrate', yaxis='y2'))
+        # Layout anpassen
+        self.fig.update_layout(
+            title='Peaks im EKG-Signal',
             xaxis_title='Zeit [s]',
-            yaxis_title='Amplitude',
-            showlegend=True)
-        return fig
+            yaxis=dict(title='Amplitude', side='left', autorange=True),
+            yaxis2=dict(title='Herzfrequenz [bpm]', overlaying='y', side='right', autorange=True),
+            showlegend=True
+        )
+        self.fig.show()
+        return self.fig
         
-            
     def load_by_id(self, id):
-        if id == self.id:
-            print(self.__dict__)
-        else:
-            print("ID not found")
-
-
-
+        persons = Person.load_person_data()
+        for persons in person_data:
+            if persons["id"] == id:
+                print({
+                "id": self.id,
+                "date": datetime.now(),
+                "result_link": "data/ekg_data/01_Ruhe.txt"
+                })
+            else:
+                print("ID not found")
 
 if __name__ == "__main__":
     print("This is a module with some functions to read the EKG data")
     file = open("data/person_db.json")
     person_data = json.load(file)
-    ekg_dict = person_data[0]["ekg_tests"][0]
-    #print(ekg_dict)
-    ekg = EKGdata(ekg_dict)
+    ekg_dict1 = person_data[0]["ekg_tests"][0]
+    ekg_dict2 = person_data[1]["ekg_tests"][0]
+    ekg_dict3 = person_data[2]["ekg_tests"][0]
+    ekg1 = EKGdata(ekg_dict1)
+    ekg2 = EKGdata(ekg_dict2)
+    ekg3 = EKGdata(ekg_dict3)
     #ekg.load_by_id(1)
     #ekg.find_peaks()
     #ekg.estimate_hr()
     #ekg.plot_time_series()
+    ekg1.load_by_id(1)
+    
